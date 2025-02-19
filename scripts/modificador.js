520d33138266cc7f9c3bba8b5a6b529c0218001f
jsillero <nuevacuentacangrejo@gmail.com>
2025-02-19 16:47:07 +0100

    Un minimo de organizacion de ficheros creado

---
+++ scripts/modificador.js
@@ -0,0 +1,16 @@
+class Modificador {
+    /**
+     * 
+     * @param {String} nombre 
+     * @param {String} modificado nombre del atributo modificado de la clase Personaje, 
+     * @param {Int} numero numero a sumar o restar 
+     * @param {Int} turnos turnos durante los que se aplica, en caso de ser -1 es permanente
+     */
+    constructor(nombre, modificado, numero, turnos) { 
+        this.nombre =nombre;
+        this.modificado = modificado;
+        this.numero = numero;
+        this.turnos = turnos;
+    }
+
+}
